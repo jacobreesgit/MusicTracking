aa8b0af1c52bdeb7af0b2fdad7f41c0c6f44eac9
jacobreesgit <jacobrees@me.com>
2025-06-27 17:26:04 +0100

    Initial Commit

---
+++ MusicTracking/MusicTrackingApp.swift
@@ -0,0 +1,17 @@
+//
+//  MusicTrackingApp.swift
+//  MusicTracking
+//
+//  Created by Jacob Rees on 27/06/2025.
+//
+
+import SwiftUI
+
+@main
+struct MusicTrackingApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
